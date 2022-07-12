65520a16cd534373ff57fef5d9de92b5987c7d98
Austin P <apedigo@moreheadstate.edu>
2022-07-12 19:07:23 -0400

    new file:   Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/389ca144-5b01-4ee0-8a35-190ea030478d.vsidx 	new file:   Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/d198645e-fc6d-4b24-b5a0-7cad142286f3.vsidx 	new file:   Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/read.lock 	new file:   Project1/LineCalculator/.vs/LineCalculator/v17/.suo 	new file:   Project1/LineCalculator/LineCalculator.sln 	new file:   Project1/LineCalculator/WindowsFormsApp1/App.config 	new file:   Project1/LineCalculator/WindowsFormsApp1/Form1.Designer.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Form1.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Form1.resx 	new file:   Project1/LineCalculator/WindowsFormsApp1/Program.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Properties/AssemblyInfo.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Properties/Resources.Designer.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Properties/Resources.resx 	new file:   Project1/LineCalculator/WindowsFormsApp1/Properties/Settings.Designer.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/Properties/Settings.settings 	new file:   Project1/LineCalculator/WindowsFormsApp1/WindowsFormsApp1.csproj 	new file:   Project1/LineCalculator/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe 	new file:   Project1/LineCalculator/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe.config 	new file:   Project1/LineCalculator/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.pdb 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/.NETFramework,Version=v4.8.AssemblyAttributes.cs 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/DesignTimeResolveAssemblyReferences.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.Properties.Resources.resources 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.Window1.resources 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.AssemblyReference.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.CoreCompileInputs.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.FileListAbsolute.txt 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.GenerateResource.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.SuggestedBindingRedirects.cache 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.exe 	new file:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.pdb

---
+++ Project1/LineCalculator/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Window1 : Form
+    {
+        int counter = 0; 
+        Point point1, point2;
+
+        public Point Point1 { get => point1; set => point1=value; }
+        public Point Point2 { get => point2; set => point2=value; }
+
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConstructLines(object sender, MouseEventArgs e, PaintEventArgs a)
+        {
+            GraphicsPath graphics = new GraphicsPath();
+            Pen drawingTool = new Pen(Color.Black, 2);
+
+            Point point1, point2;
+
+            point1 = MousePosition;
+            point2 = MousePosition;
+
+            graphics.AddLine(point1, point2);
+            a.Graphics.DrawPath(drawingTool, graphics);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Window1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (counter == 0)
+            {
+                Point1 = MousePosition;
+                counter++;
+            }
+            else 
+            {
+                Point2 = MousePosition;
+            }
+        }
+
+        //function works
+        private void Window1_MouseClick(object sender, System.EventArgs e)
+        {
+            if (counter == 0)
+            {
+                Point1 = MousePosition;
+                label1.Text = Convert.ToString(Point1); 
+                counter++;
+            }
+            else
+            {
+                Point2 = MousePosition;
+                label2.Text = Convert.ToString(point2);
+            }
+        }
+
+        //assume issue lies here?
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath graphics = new GraphicsPath();
+            Pen drawingTool = new Pen(Color.Black, 2);
+
+            e.Graphics.DrawLine(drawingTool, point1, point2);
+        }
+    }
+}
