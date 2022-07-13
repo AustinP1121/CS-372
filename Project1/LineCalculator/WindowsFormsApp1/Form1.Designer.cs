eac36741d683ee59eb4eef294b44e1324b387638
Austin P <apedigo@moreheadstate.edu>
2022-07-13 00:41:09 -0400

    deleted:    Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/389ca144-5b01-4ee0-8a35-190ea030478d.vsidx 	deleted:    Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/d198645e-fc6d-4b24-b5a0-7cad142286f3.vsidx 	new file:   Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/df749428-9977-48ec-8a81-65ede5eab8e1.vsidx 	new file:   Project1/LineCalculator/.vs/LineCalculator/FileContentIndex/fb4d49b5-097a-4136-9f58-ac5a3c712115.vsidx 	modified:   Project1/LineCalculator/.vs/LineCalculator/v17/.suo 	modified:   Project1/LineCalculator/WindowsFormsApp1/Form1.Designer.cs 	modified:   Project1/LineCalculator/WindowsFormsApp1/Form1.cs 	modified:   Project1/LineCalculator/WindowsFormsApp1/Form1.resx 	modified:   Project1/LineCalculator/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.exe 	modified:   Project1/LineCalculator/WindowsFormsApp1/bin/Debug/WindowsFormsApp1.pdb 	modified:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.csproj.GenerateResource.cache 	modified:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.exe 	modified:   Project1/LineCalculator/WindowsFormsApp1/obj/Debug/WindowsFormsApp1.pdb

--- Project1/LineCalculator/WindowsFormsApp1/Form1.Designer.cs
+++ Project1/LineCalculator/WindowsFormsApp1/Form1.Designer.cs
@@ -28,63 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panel1
             // 
-            this.button1.Location = new System.Drawing.Point(184, 376);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(6, 7, 8, 8);
+            this.panel1.Size = new System.Drawing.Size(496, 519);
+            this.panel1.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Window1_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(472, 382);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter Your Four Points Here";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(294, 129);
+            this.label1.Location = new System.Drawing.Point(204, 420);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Point 1 Coordinates: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(204, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Point 4 Coordinates: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(68, 437);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 410);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(204, 446);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Point 3 Coordinates: ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(294, 173);
+            this.label2.Location = new System.Drawing.Point(204, 433);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Point 2 Coordinates: ";
             // 
             // Window1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.ClientSize = new System.Drawing.Size(456, 433);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(496, 519);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Window1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Line Calculator";
-            this.Click += new System.EventHandler(this.Window1_MouseClick);
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Window1_MouseClick);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- Project1/LineCalculator/WindowsFormsApp1/Form1.cs
+++ Project1/LineCalculator/WindowsFormsApp1/Form1.cs
@@ -8,33 +8,27 @@ namespace WindowsFormsApp1
     public partial class Window1 : Form
     {
         int counter = 0; 
-        Point point1, point2;
+        private Point point1, point2, point3,
+            point4;
 
         public Point Point1 { get => point1; set => point1=value; }
         public Point Point2 { get => point2; set => point2=value; }
+        public Point Point3 { get => point3; set => point3=value; }
+        public Point Point4 { get => point4; set => point4=value; }
 
         public Window1()
         {
             InitializeComponent();
         }
 
-        private void ConstructLines(object sender, MouseEventArgs e, PaintEventArgs a)
-        {
-            GraphicsPath graphics = new GraphicsPath();
-            Pen drawingTool = new Pen(Color.Black, 2);
-
-            Point point1, point2;
-
-            point1 = MousePosition;
-            point2 = MousePosition;
-
-            graphics.AddLine(point1, point2);
-            a.Graphics.DrawPath(drawingTool, graphics);
-        }
-
         private void button1_Click(object sender, System.EventArgs e)
         {
-            this.Invalidate();
+            Paint += new PaintEventHandler(pictureBox1_Paint);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
         }
 
         private void Window1_MouseClick(object sender, MouseEventArgs e)
@@ -42,37 +36,51 @@ namespace WindowsFormsApp1
             if (counter == 0)
             {
                 Point1 = MousePosition;
+                label1.Text = "Point 1 Coordinates: "+Convert.ToString(point1);
+                counter++;
+            }
+            else if (counter == 1)
+            {
+                Point2 = MousePosition;
+                label2.Text = "Point 2 Coordinates: "+Convert.ToString(point2);
+                counter++;
+            }
+            else if(counter == 2)
+            {
+                Point3 = MousePosition;
+                label3.Text = "Point 3 Coordinates: "+Convert.ToString(point3);
+                counter++;
+            }
+            else if( counter == 3)
+            {
+                Point4 = MousePosition;
+                label4.Text = "Point 4 Coordinates: "+Convert.ToString(point4);
                 counter++;
             }
             else 
-            {
-                Point2 = MousePosition;
+            { 
+                //do nothing
             }
         }
 
-        //function works
-        private void Window1_MouseClick(object sender, System.EventArgs e)
-        {
-            if (counter == 0)
-            {
-                Point1 = MousePosition;
-                label1.Text = Convert.ToString(Point1); 
-                counter++;
-            }
-            else
-            {
-                Point2 = MousePosition;
-                label2.Text = Convert.ToString(point2);
-            }
-        }
-
-        //assume issue lies here?
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            GraphicsPath graphics = new GraphicsPath();
-            Pen drawingTool = new Pen(Color.Black, 2);
+            int pX1 = point1.X;
+            int pY1 = point1.Y;
 
-            e.Graphics.DrawLine(drawingTool, point1, point2);
+            int pX2 = point2.X;
+            int pY2 = point2.Y;
+
+            int pX3 = point3.X;
+            int pY3 = point3.Y;
+
+            int pX4 = point4.X;
+            int pY4 = point4.Y; 
+
+            Pen drawingTool = new Pen(Color.Black, 4);
+            e.Graphics.DrawLine(drawingTool, pX1, pY1, pX2, pY2);
+            e.Graphics.DrawLine(drawingTool, pX3, pY3, pX4, pY4);
+
         }
     }
 }
